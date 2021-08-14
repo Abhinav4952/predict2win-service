546f23e4a2e69d919190d90788553511afde5008
Abhinav Veeramosu <abhinav.veeramosu@ehealthCorp.net>
2021-08-14 22:35:37 +0530

    Implement login through google

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -17,4 +17,6 @@ router.route("/forgotpassword").post(forgotPassword);
 
 router.route("/passwordreset/:resetToken").put(resetPassword);
 
+router.route("/socialLogin/").post(resetPassword);
+
 module.exports = router;
